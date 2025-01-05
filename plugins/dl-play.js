749395b676620ce83f7177f3080c4ce03fd08a41
olagente38 <benjaminmijahuanga@gmail.com>
2025-01-05 11:11:47 -0500

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -2,7 +2,7 @@
 - Play Botones By Angel-OFC 
 - https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
 */
-import fetch from 'node-fetch';
+/*import fetch from 'node-fetch';
 import yts from 'yt-search';
 
 let handler = async (m, { conn, args }) => {
