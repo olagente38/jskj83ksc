46d6d459de8904cdfce4ae9692a482fa1556d1b2
olagente38 <benjaminmijahuanga@gmail.com>
2025-01-05 10:55:14 -0500

    Update dl-ytmp3.js

--- plugins/dl-ytmp3.js
+++ plugins/dl-ytmp3.js
@@ -14,14 +14,26 @@ let handler = async (m, { conn, text }) => {
   try {
     let api = await fetch(`https://api.zenkey.my.id/api/download/ytmp3?apikey=zenkey&url=${text}`)
     let json = await api.json()
+    
+    // Verifica si el resultado es válido antes de continuar
+    if (!json.result || !json.result.download_url) {
+      return m.reply("❀ No se pudo obtener el archivo de audio. Intenta con otra URL.")
+    }
+
     let { quality, title, download_url } = json.result
 
     await m.react('✅')
-    await conn.sendMessage(m.chat, { 
-      audio: { url: download_url }, 
-      fileName: `${title}.mp3`, 
-      mimetype: 'audio/mp4' 
-    }, { quoted: m })
+
+    // Verifica que la URL de descarga no esté vacía antes de enviar el mensaje
+    if (download_url) {
+      await conn.sendMessage(m.chat, { 
+        audio: { url: download_url }, 
+        fileName: `${title}.mp3`, 
+        mimetype: 'audio/mp4' 
+      }, { quoted: m })
+    } else {
+      m.reply("❀ La URL de descarga es inválida o no se pudo procesar.")
+    }
   } catch (error) {
     console.error(error)
     m.reply("❀ Hubo un error al procesar la URL. Inténtalo nuevamente.")
