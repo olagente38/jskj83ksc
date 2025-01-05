1847970bd198f26dc7ab2ef92f06632852e79ceb
olagente38 <benjaminmijahuanga@gmail.com>
2025-01-04 21:59:46 -0500

    Update rpg-levelup.js

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -16,11 +16,11 @@ let before = user.level * 1
 while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
 if (before !== user.level) {
 let txt = `🎊 F E L I C I T A C I O N E S 🎊\n\n` 
-txt += `*${before}* ➔ *${user.level}* [ ${user.role} ]\n\n`
+txt += `*${before}* ➔ *${user.level}*\n\n`
 txt += `• 🧬 Nivel anterior : ${before}\n`
 txt += `• 🧬 Nuevos niveles : ${user.level}\n`
 txt += `• 📅 Fecha : ${new Date().toLocaleString('id-ID')}\n\n`
-txt += `🚩 *Nota:* _Cuanto más a menudo interactúes con *Ai Yaemori*, mayor será tu nivel_`
+txt += `🚩 *Nota:* _Cuanto más a menudo interactúes con *Mizuki Ai*, mayor será tu nivel_`
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)}}
 
 handler.help = ['levelup']
