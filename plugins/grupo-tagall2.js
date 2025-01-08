aa74638ac290a98372c3e6f3020e06dcbb864c5e
olagente38 <benjaminmijahuanga@gmail.com>
2025-01-08 01:54:59 -0500

    Update grupo-tagall2.js

--- plugins/grupo-tagall2.js
+++ plugins/grupo-tagall2.js
@@ -16,18 +16,18 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, co
 
   const pesan = args.join` `;
   const oi = `*» INFO :* ${pesan}`;
-  let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄  ۪꒰ \`Mizuki Ai\` ꒱  ۟  𝅄 ┄\n`;
+  let teks = `𝐈𝐍𝐓𝐄𝐆𝐑𝐀𝐍𝐓𝐄𝐒 𝐃𝐄𝐋 𝐂𝐋𝐀𝐍\n\n*╭ ┄ ┄ ┄ ─* *꒰* \`𝐂𝐈𝐁𝐄𝐑 𝐁𝐀𝐍𝐃𝐎 𝟐𝐘𝐊\` *꒱* *┄  ─ ┄*\n`;
   for (const mem of participants) {
-    teks += `┊${customEmoji} @${mem.id.split('@')[0]}\n`;
+    teks += `*┊*👑 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄ ⸼`;
+  teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *ᴄɪʙᴇʀ ʙᴀɴᴅᴏ 2ʏᴋ* ୧ ׅ ꒱  ┄  ─ ┄ ⸼`;
 
   conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) });
 };
 
 handler.help = ['invocary2k *<mensaje opcional>*'];
 handler.tags = ['groupy2k'];
-handler.command = /^(tagally2k|invocary2k|clanY2K|clany2k|invocarY2K)$/i;
+handler.command = /^(tagall2yk|invocar2yk|clan2YK|clan2Yk|invocar2YK)$/i;
 handler.admin = true;
 handler.group = true;
 
