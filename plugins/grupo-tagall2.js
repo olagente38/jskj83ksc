ccdd3cc5b769dec5f1316961a278396350d9b8cc
olagente38 <benjaminmijahuanga@gmail.com>
2025-01-08 01:58:17 -0500

    Update grupo-tagall2.js

--- plugins/grupo-tagall2.js
+++ plugins/grupo-tagall2.js
@@ -16,7 +16,7 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, co
 
   const pesan = args.join` `;
   const oi = `*» INFO :* ${pesan}`;
-  let teks = `𝐋𝐈𝐒𝐓𝐀 𝐃𝐄\n𝐈𝐍𝐓𝐄𝐆𝐑𝐀𝐍𝐓𝐄𝐒 𝐃𝐄𝐋 𝐂𝐋𝐀𝐍\n\n*╭ ┄ ┄* *꒰* \`𝐂𝐈𝐁𝐄𝐑 𝐁𝐀𝐍𝐃𝐎 𝟐𝐘𝐊\` *꒱* *┄ ┄*\n`;
+  let teks = `𝐋𝐈𝐒𝐓𝐀 𝐃𝐄\n𝐈𝐍𝐓𝐄𝐆𝐑𝐀𝐍𝐓𝐄𝐒\n𝐃𝐄𝐋 𝐂𝐋𝐀𝐍\n\n*╭ ┄ ┄* *꒰* \`𝐂𝐈𝐁𝐄𝐑 𝐁𝐀𝐍𝐃𝐎 𝟐𝐘𝐊\` *꒱* *┄ ┄*\n`;
   for (const mem of participants) {
     teks += `*┊*👑 @${mem.id.split('@')[0]}\n`;
   }
