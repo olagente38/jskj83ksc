fb55f2bca6bf8d88b84a09762953acf5af5ff90d
olagente38 <benjaminmijahuanga@gmail.com>
2025-01-08 02:32:05 -0500

    Update serbot-permiso.js

--- plugins/serbot-permiso.js
+++ plugins/serbot-permiso.js
@@ -3,7 +3,7 @@ var handler = async m =>
 conn.reply(m.chat, ` 
 \`📢 𝐂𝐎𝐌𝐔𝐍𝐈𝐂𝐀𝐃𝐎 📢\`
 
-*[🚨]* *»* El comando *".serbot"* está activo, pero se ha cambiado el nombre del comando.\n\n*[📝]* *»* Para saber cual es el comando correcto y convertirte en un sub bot, primero debes pedir permiso a mi creador.\n\n*[✅]* *»* Para obtener su contacto, escribe el comando ".owner"
+*[🚨]* *»* El comando *".serbot"* está activo, pero se ha cambiado el nombre del comando.\n\n*[📝]* *»* Para saber cual es el comando correcto y convertirte en un sub bot, primero debes pedir permiso a mi creador.\n\n*[✅]* *»* Para obtener su contacto, escribe el comando *".owner"*
  `, m, rcanal, )
 
 handler.help = ['serbot']
